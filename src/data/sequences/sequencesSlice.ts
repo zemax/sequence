ed2bba5a4915d61ee08e1fb673968a70d5d94cf5
Maxime Cousinou <maxime.cousinou@zemax.net>
2026-09-06 12:13:58 +0200

    feat(exemples): add default examples

--- src/data/sequences/sequencesSlice.ts
+++ src/data/sequences/sequencesSlice.ts
@@ -22,20 +22,39 @@ export interface Sequence {
   items: SequenceItem[];
 }
 
+const initialSequences: Sequence[] = [
+  {
+    id: "1",
+    name: "Exercices quotidien",
+    items: [
+      { id: "1", type: "pause", title: "Appuyez quand vous êtes prêt" },
+      { id: "2", type: "countdown", title: "C'est parti pour la planche, bras gauche au sol", duration: 5 },
+      { id: "3", type: "countdown", title: "On tient la planche bras gauche", duration: 30 },
+      { id: "4", type: "countdown", title: "Repos", duration: 30 },
+      { id: "5", type: "countdown", title: "On tient la planche bras droit", duration: 30 },
+      { id: "6", type: "countdown", title: "Repos", duration: 30 },
+      { id: "7", type: "countdown", title: "On tient la planche bras gauche", duration: 45 },
+      { id: "8", type: "countdown", title: "Repos", duration: 30 },
+      { id: "9", type: "countdown", title: "On tient la planche bras droit", duration: 45 },
+      { id: "10", type: "pause", title: "Bravo" },
+    ],
+  },
+  {
+    id: "2",
+    name: "Recette des œufs mollets",
+    items: [
+      { id: "11", type: "pause", title: "Faites bouillir de l'eau" },
+      { id: "12", type: "pause", title: "Plongez les œufs dans l'eau" },
+      { id: "13", type: "countdown", title: "Cuisson", duration: 330 },
+      { id: "14", type: "pause", title: "Sortez la casserole du feu et remplacez l'eau par de l'eau froide" },
+      { id: "15", type: "pause", title: "Écalez les œufs délicatement, sous un filet d'eau froide" },
+    ],
+  },
+];
+
 export const sequencesSlice = createSlice({
   name: "sequences",
-  initialState: [
-    {
-      id: "1",
-      name: "Sequence 1",
-      items: [],
-    },
-    {
-      id: "2",
-      name: "Sequence 2",
-      items: [],
-    },
-  ],
+  initialState: initialSequences,
   reducers: {
     addSequence: (state, action) => {
       state.push(action.payload);
